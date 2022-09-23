c240a6ec7578934396ff97693d2ea0cae263c26f
Tinh1308 <tinhdm@flydinotech.com>
2022-09-23 07:19:08 +0700

    login

---
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * This is auth Controllers using for authentication user
+ *
+ * @package App\Http\Controllers\Api\User
+ */
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\Api\User\RegisterRequest;
+use App\Services\Api\User\AuthService;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+/**
+ * Class AuthController
+ */
+class AuthController extends Controller
+{
+    /**
+     * Constructor.
+     *
+     * @param AuthService $authService Authentication service.
+     */
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    /**
+     * Register user with email and password.
+     *
+     * @param RegisterRequest $request Request object.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+            $register = $this->authService
+                ->register($request->validated());
+            if ($register['code'] != Response::HTTP_OK) {
+                return response()->apiErrors($register);
+            }
+
+            DB::commit();
+
+            return response()->apiSuccess($register);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            DB::rollback();
+
+            return response()->apiErrors(
+                [
+                    'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                    'message' => __('auth.register_error'),
+                ]
+            );
+        }//end try
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            Config::set('jwt.user', 'App\User');
+            Config::set('auth.providers.users.model', \App\User::class);
+            $token = null;
+
+            if ($token = JWTAuth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                return response()->json([
+                    'code' => Response::HTTP_OK,
+                    'response' => __('auth.success'),
+                    'result' => [
+                        'token' => $token,
+                    ],
+                ], 200);
+            } else {
+                return response()->json([
+                    'code' => Response::HTTP_BAD_REQUEST,
+                    'response' => 'error',
+                    'message' => __('auth.login_error'),
+                ], 400);
+            }
+
+            DB::commit();
+
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            DB::rollback();
+
+            return response()->json(
+                [
+                    'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                    'message' => __('auth.login_error'),
+                ], 500
+            );
+        }//end try
+
+    }
+
+    public function logout()
+    {
+        if(Auth::check() == false)
+        {
+            return response()->json([
+                'code' => Response::HTTP_UNAUTHORIZED,
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        Auth::guard('api')->logout();
+        return response()->json([
+            'message' => __('auth.logout_success'),
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::POST('register', [AuthController::class, 'register'])->name('register');
+
+Route::middleware('auth:api')->get('/user', function(Request $request){
+    return $request->user();
+});
