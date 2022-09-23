a8a090c32bb0c1c2d4bbe9e9baab050e830a696a
Tinh1308 <tinhdm@flydinotech.com>
2022-09-24 00:58:24 +0700

    fix request validate Login

--- app/Http/Requests/Api/User/LoginRequest.php
+++ app/Http/Requests/Api/User/LoginRequest.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Requests\api\User;
 
+use App\Http\Requests\Api\User\BaseRequest;
 use Illuminate\Foundation\Http\FormRequest;
 
-class LoginRequest extends FormRequest
+class LoginRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize()
-    {
-        return true;
-    }
+    // public function authorize()
+    // {
+    //     return true;
+    // }
 
     /**
      * Get the validation rules that apply to the request.
