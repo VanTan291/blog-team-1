c7120bce207ccca3d3d84f62779636aa9f0a0cb9
Lâm Văn Tân <lamvantan@MacBook-Pro-cua-Lam.local>
2022-09-25 22:55:02 +0700

    hot fix

--- app/Http/Requests/Api/User/LoginRequest.php
+++ app/Http/Requests/Api/User/LoginRequest.php
@@ -1,22 +1,11 @@
 <?php
 
-namespace App\Http\Requests\api\User;
+namespace App\Http\Requests\Api\User;
 
-use App\Http\Requests\Api\User\BaseRequest;
-use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class LoginRequest extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    // public function authorize()
-    // {
-    //     return true;
-    // }
-
     /**
      * Get the validation rules that apply to the request.
      *
