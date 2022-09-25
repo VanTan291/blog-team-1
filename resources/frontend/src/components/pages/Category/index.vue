247cc4c6aee6332b1fecf1d59a606197ff91bbd9
alethanhson <sonalt@flydinotech.com>
2022-09-25 14:54:03 +0700

    category

--- resources/frontend/src/components/pages/Category/index.vue
+++ resources/frontend/src/components/pages/Category/index.vue
@@ -14,7 +14,7 @@
                         <div class="col-md-12">
                             <div class="input-group input-group-outline my-3">
                                 <input type="text" v-model="category.name" class="form-control"
-                                    placeholder="category name">
+                                    placeholder="Tên Danh Mục">
                                 <input type="hidden" v-model="category.id">
                             </div>
                         </div>
@@ -22,10 +22,13 @@
                     <div class="form-check form-switch">
                         <input class="form-check-input" type="checkbox" v-model="category.status"
                             v-bind:id="category.status">
-                        <label class="form-check-label" for="flexSwitchCheckDefault">Status</label>
+                        <label class="form-check-label" for="flexSwitchCheckDefault">Trạng thái</label>
                     </div>
-                    <button type="submit" class="btn-slack btn btn-icon align-self-end">
-                        <span class="btn-inner--text">add new</span>
+                    <button type="submit" class="btn bg-gradient-info w-auto me-2 align-self-end" v-if="category.id">
+                        <span class="btn-inner--text">Cập nhật</span>
+                    </button>
+                    <button type="submit" class="btn-slack btn btn-icon align-self-end" v-else>
+                        <span class="btn-inner--text">Thêm mới</span>
                     </button>
                 </form>
             </div>
@@ -36,13 +39,13 @@
                             <thead>
                                 <tr>
                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        category name</th>
+                                        Tên Danh Mục</th>
                                     <th
                                         class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                        status</th>
+                                        Trạng Thái</th>
                                     <th
                                         class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        action</th>
+                                        Hành Động</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -61,11 +64,11 @@
                                     <td class="d-flex justify-content-center">
                                         <button type="button" class="btn btn-facebook my-0 mx-1 p-2 f-item"
                                             @click="show(cate.id)">
-                                            <span class="btn-inner--text">edit</span>
+                                            <span class="btn-inner--text">Sửa</span>
                                         </button>
                                         <button type="button" class="btn btn-youtube my-0 mx-1 p-2 f-item"
                                             @click="destroy(cate.id)">
-                                            <span class="btn-inner--text">delete</span>
+                                            <span class="btn-inner--text">Xóa</span>
                                         </button>
                                     </td>
                                 </tr>
@@ -121,9 +124,9 @@ export default {
                             name: '',
                             status: true,
                         }
-                        this.toastSuccess('Update category success');
+                        this.toastSuccess('Cập nhật thành công');
                     } else {
-                        this.toastSuccess('Update category fail');
+                        this.toastSuccess('Cập nhật thất bại');
                         this.errors = result;
                         console.log(result);
                     }
@@ -138,9 +141,9 @@ export default {
                             name: '',
                             status: true,
                         }
-                        this.toastSuccess('Create category success');
+                        this.toastSuccess('Tạo mới thành công');
                     } else {
-                        this.toastSuccess('Create category fail');
+                        this.toastSuccess('Tạo mới thất bại');
                         console.log(result);
                     }
                 }).catch(error => {
@@ -163,19 +166,19 @@ export default {
 
         destroy(category) {
             this.$confirm({
-                    message: 'Are you sure?',
+                    message: 'Bạn có chắc chắn muốn xóa?',
                     button: {
-                        no: 'No',
-                        yes: 'Yes'
+                        no: 'Không',
+                        yes: 'Đúng'
                     },
                     callback: confirm => {
                         if (confirm) {
                             this.deleteCategory(category).then(result => {
                                 if (result.data.code == 200) {
                                     this.getListCategories(this.page);
-                                    this.toastSuccess('Delete category success');
+                                    this.toastSuccess('Xóa thành công');
                                 } else {
-                                    this.toastSuccess('Delete category fail');
+                                    this.toastSuccess('Xóa thất bại');
                                     console.log(result);
                                 }
                             }).catch(error => {
